afa5b6d240e0653a43e5baa5894427262e3e88e0
Benjamin Morgan <benjamin.morgan@bosch.com>
2021-01-08 13:09:53 +0100

    cli: Pass extra arguments to shell command

--- cli/cloe_launch/exec.py
+++ cli/cloe_launch/exec.py
@@ -158,7 +158,9 @@ class Environment:
                 self._data[kv[0]] = kv[1]
             except IndexError:
                 logging.error(
-                    "Error: cannot interpret environment key-value pair:", line
+                    "Error: cannot interpret environment key-value pair: {}".format(
+                        line
+                    )
                 )
 
     def __delitem__(self, key: str):
@@ -508,6 +510,7 @@ class Engine:
 
     def shell(
         self,
+        arguments: List[str] = None,
         use_cache: bool = False,
     ) -> None:
         """Launch a SHELL with the environment variables adjusted."""
@@ -545,7 +548,10 @@ class Engine:
 
         # Replace this process with the SHELL now.
         sys.stdout.flush()
-        os.execvpe(shell, [shell], env.as_dict())
+        cmd = [shell]
+        if arguments is not None:
+            cmd.extend(arguments)
+        os.execvpe(shell, cmd, env.as_dict())
 
     def exec(
         self,
